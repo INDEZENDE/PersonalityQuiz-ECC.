b46f0df02852b5a0df61c331252dda3f0eab8423
INDEZENDE <i.rios.zendejas@gmail.com>
2019-10-12 14:30:02 -0500

    Creando repo

---
+++ PersonalityQuiz-ECC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz-ECC
+//
+//  Created by Apple Device 13 on 10/12/19.
+//  Copyright © 2019 Las jajas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
